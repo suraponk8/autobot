75d0892abeca3b50218fcb9c0aaecb8aa72b0da7
OFFICEMATE\surapon <surapon@officemate.co.th>
2019-03-26 01:37:56 +0700

    Initial

--- index.php
+++ index.php
@@ -1,2 +1,46 @@
 <?php
-echo 'ok';
+/**
+ * Use for return easy answer.
+ */
+require_once('./vendor/autoload.php');
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+$channel_token = 'ysfRZCq8e4OS0mGNVnoYgQA89ZIN3FOb3OR/8bkQIJO1u4gvsfIAI937531nGsLPgBHncR/hTI6AmtaVaxeQqPs8BkqkqxCzx+1eRYixbwPoLq05cUtOJjBVlsDCEDjRrd22hVT6wLONgwsawtTcUAdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'e28097db44cf5610723dd820c3b8b27a';
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+    
+        // Line API send a lot of event type, we interested in message only.
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+            switch($event['message']['text']) {
+                
+                case 'tel':
+                    $respMessage = '089-5124512';
+                    break;
+                case 'address':
+                    $respMessage = '99/451 Muang Nonthaburi';
+                    break;
+                case 'boss':
+                    $respMessage = '089-2541545';
+                    break;
+                case 'idcard':
+                    $respMessage = '5845122451245';
+                    break;
+                default:
+                    break;
+            }
+            $httpClient = new CurlHTTPClient($channel_token);
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+            $textMessageBuilder = new TextMessageBuilder($respMessage);
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+		}
+	}
+}
+echo "OK";
