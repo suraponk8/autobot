ea8caba02735d7fe58f40e630df0feaf8fced7ab
OFFICEMATE\surapon <surapon@officemate.co.th>
2019-03-26 01:45:48 +0700

    V1

--- index.php
+++ index.php
@@ -1,46 +1,67 @@
 <?php
-/**
- * Use for return easy answer.
+/*
+ {
+  "events": [
+      {
+        "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+        "type": "message",
+        "timestamp": 1462629479859,
+        "source": {
+             "type": "user",
+             "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+         },
+         "message": {
+             "id": "325708",
+             "type": "text",
+             "text": "Hello, world"
+          }
+      }
+  ]
+}
  */
+
 require_once('./vendor/autoload.php');
+
+// Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+// Token
 $channel_token = 'ysfRZCq8e4OS0mGNVnoYgQA89ZIN3FOb3OR/8bkQIJO1u4gvsfIAI937531nGsLPgBHncR/hTI6AmtaVaxeQqPs8BkqkqxCzx+1eRYixbwPoLq05cUtOJjBVlsDCEDjRrd22hVT6wLONgwsawtTcUAdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'e28097db44cf5610723dd820c3b8b27a';
+
 // Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
+
 if (!is_null($events['events'])) {
+
 	// Loop through each event
 	foreach ($events['events'] as $event) {
     
         // Line API send a lot of event type, we interested in message only.
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-            // Get replyToken
-            $replyToken = $event['replyToken'];
-            switch($event['message']['text']) {
+		if ($event['type'] == 'message') {
+
+            switch($event['message']['type']) {
                 
-                case 'tel':
-                    $respMessage = '089-5124512';
-                    break;
-                case 'address':
-                    $respMessage = '99/451 Muang Nonthaburi';
-                    break;
-                case 'boss':
-                    $respMessage = '089-2541545';
-                    break;
-                case 'idcard':
-                    $respMessage = '5845122451245';
-                    break;
-                default:
+                case 'text':
+                    // Get replyToken
+                    $replyToken = $event['replyToken'];
+        
+                    // Reply message
+                    $respMessage = 'Hello, your message is '. $event['message']['text'];
+            
+                    $httpClient = new CurlHTTPClient($channel_token);
+                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+        
+                    $textMessageBuilder = new TextMessageBuilder($respMessage);
+                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                    
                     break;
             }
-            $httpClient = new CurlHTTPClient($channel_token);
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-            $textMessageBuilder = new TextMessageBuilder($respMessage);
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 		}
 	}
 }
+
 echo "OK";
